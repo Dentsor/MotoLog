091ba19a309e0a941ddbe7f24a29bce90ea69ce5
Dentsor <dentsor@dentsor.net>
2023-09-24 15:14:12 +0200

    feat: Implement autocomplete field for Fuel Station
    Refs: #19

--- lib/widgets/refuel_form.dart
+++ lib/widgets/refuel_form.dart
@@ -1,10 +1,12 @@
 import 'package:date_field/date_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:intl/intl.dart';
 import 'package:motolog/database/database_helper.dart';
 import 'package:motolog/database/refuel_db_helper.dart';
 import 'package:motolog/models/refuel.dart';
 import 'package:motolog/models/vehicle.dart';
+import 'package:motolog/utils/refuel_utils.dart';
 import 'package:motolog/widgets/checkbox_form_field.dart';
 
 class RefuelForm extends StatefulWidget {
@@ -125,144 +127,187 @@ class _RefuelEditForm extends State<RefuelForm> {
     if (saveState == 1) {
       return const CircularProgressIndicator();
     }
+
     if (saveState == 2) {
       Navigator.pop(context);
       return const CircularProgressIndicator(); // TODO?
     }
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        title: Text(
-            '${widget.refuel == null ? 'Add' : 'Edit'} Refueling | ${widget.vehicle.name}'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: Column(
-              children: <Widget>[
-                DateTimeFormField(
-                  decoration: const InputDecoration(labelText: 'Date and Time'),
-                  mode: DateTimeFieldPickerMode.dateAndTime,
-                  autovalidateMode: AutovalidateMode.always,
-                  initialValue: _dateTime,
-                  dateFormat: DateFormat('yyyy-MM-dd HH:mm:ss'),
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                  onDateSelected: (value) {
-                    setState(() {
-                      _dateTime = value;
-                    });
-                  },
-                ),
-                TextFormField(
-                  controller: _stationController,
-                  decoration: const InputDecoration(labelText: 'Fuel Station'),
-                  keyboardType: TextInputType.text,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _quantityController,
-                  decoration: const InputDecoration(
-                      labelText: 'Quantity (${Refuel.fuelUnit})'),
-                  keyboardType: TextInputType.number,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    if (double.tryParse(value) == null) {
-                      return 'Enter a valid number';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _paidController,
-                  decoration: const InputDecoration(
-                      labelText: 'Paid (${Refuel.currency})'),
-                  keyboardType: TextInputType.number,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    if (double.tryParse(value) == null) {
-                      return 'Please enter a number';
-                    }
-                    if (double.parse(value) < 0) {
-                      return 'Please enter a positive number';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _distanceController,
-                  decoration: const InputDecoration(
-                      labelText: 'Distance (${Refuel.distanceUnit})'),
-                  keyboardType: TextInputType.number,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a value';
-                    }
-                    if (double.tryParse(value) == null) {
-                      return 'Please enter a number';
-                    }
-                    if (double.parse(value) < 0) {
-                      return 'Please enter a positive number';
-                    }
-                    return null;
-                  },
-                ),
-                CheckboxFormField(
-                  title: const Text('Refueled to capacity'),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  initialValue: _filledToCapacity,
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _filledToCapacity = value ?? false,
-                ),
-                CheckboxFormField(
-                  title: const Text('Missing previous entry'),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  initialValue: _missingPreviousEntry,
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please enter a value';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) => _missingPreviousEntry = value ?? false,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: ElevatedButton.icon(
-                    icon: const Icon(Icons.save),
-                    onPressed: onSubmit,
-                    label: const Text('Save'),
+
+    return FutureBuilder(
+        future: dbHelper.retrieveRefuels(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const CircularProgressIndicator();
+          }
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          if (snapshot.hasData) {
+            var refuels = snapshot.data!;
+            var stations = refuels.map((e) => e.station).toSet().toList();
+            stations.sort();
+
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                title: Text(
+                    '${widget.refuel == null ? 'Add' : 'Edit'} Refueling | ${widget.vehicle.name}'),
+              ),
+              body: Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: Column(
+                      children: <Widget>[
+                        DateTimeFormField(
+                          decoration:
+                              const InputDecoration(labelText: 'Date and Time'),
+                          mode: DateTimeFieldPickerMode.dateAndTime,
+                          autovalidateMode: AutovalidateMode.always,
+                          initialValue: _dateTime,
+                          dateFormat: DateFormat('yyyy-MM-dd HH:mm:ss'),
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please enter a value';
+                            }
+                            return null;
+                          },
+                          onDateSelected: (value) {
+                            setState(() {
+                              _dateTime = value;
+                            });
+                          },
+                        ),
+                        TypeAheadFormField(
+                          textFieldConfiguration: TextFieldConfiguration(
+                            controller: _stationController,
+                            decoration: const InputDecoration(
+                              labelText: 'Fuel Station',
+                            ),
+                            keyboardType: TextInputType.text,
+                          ),
+                          suggestionsCallback: (pattern) {
+                            return stations.where((element) => element
+                                .toLowerCase()
+                                .contains(pattern.toLowerCase()));
+                          },
+                          itemBuilder: (context, suggestion) => ListTile(
+                            title: Text(suggestion),
+                          ),
+                          hideOnEmpty: true,
+                          transitionBuilder:
+                              (context, suggestionsBox, controller) {
+                            return suggestionsBox;
+                          },
+                          onSuggestionSelected: (suggestion) {
+                            _stationController.text = suggestion;
+                          },
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _quantityController,
+                          decoration: const InputDecoration(
+                              labelText: 'Quantity (${Refuel.fuelUnit})'),
+                          keyboardType: TextInputType.number,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Enter a valid number';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _paidController,
+                          decoration: const InputDecoration(
+                              labelText: 'Paid (${Refuel.currency})'),
+                          keyboardType: TextInputType.number,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Please enter a number';
+                            }
+                            if (double.parse(value) < 0) {
+                              return 'Please enter a positive number';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _distanceController,
+                          decoration: const InputDecoration(
+                              labelText: 'Distance (${Refuel.distanceUnit})'),
+                          keyboardType: TextInputType.number,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a value';
+                            }
+                            if (double.tryParse(value) == null) {
+                              return 'Please enter a number';
+                            }
+                            if (double.parse(value) < 0) {
+                              return 'Please enter a positive number';
+                            }
+                            return null;
+                          },
+                        ),
+                        CheckboxFormField(
+                          title: const Text('Refueled to capacity'),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          initialValue: _filledToCapacity,
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please enter a value';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) =>
+                              _filledToCapacity = value ?? false,
+                        ),
+                        CheckboxFormField(
+                          title: const Text('Missing previous entry'),
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          initialValue: _missingPreviousEntry,
+                          validator: (value) {
+                            if (value == null) {
+                              return 'Please enter a value';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) =>
+                              _missingPreviousEntry = value ?? false,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: ElevatedButton.icon(
+                            icon: const Icon(Icons.save),
+                            onPressed: onSubmit,
+                            label: const Text('Save'),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                ),
+              ),
+            );
+          }
+          return const Text('No data');
+        });
   }
 }
